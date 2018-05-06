0c9f0c947b97cf7471f511078ab13c6a89c99eae
Victor <viclee02@gmail.com>
2018-05-05 20:22:12 -1000

    adds new exercises

---
+++ exercises.py
@@ -0,0 +1,72 @@
+#Comments are text that will not be executed in your code. Rather comments are for other programmers to read. 
+
+# All of the exercises below are commented out. Write your Javascript code after each exercise.
+
+#Variables and Data Types 
+#Print each variable and test your code in the terminal using the Python interpreter
+
+#example
+greeting = 'Hello Prepster';
+print(greeting); #this should print 'Hello Prepster'
+
+#1 Variables with a String value
+#Declare variables named first_name, last_name, birth_place, hobby, role_model, famous_quote, fav_president, fav_food, fav_color, fav_song
+
+#Assign your own string values to each variable and print each variable.
+
+#2 String Concatenation
+#Declare a variable named full_name and concatenate first_name and last_name. Print the full_name variable.
+
+#Declare a variable named intro and prints the following sentence:
+#'Hello, my name is x and I was born in y.' Where x is full_name and y is birth_place. 
+
+#Declare a variable named about_me and prints the following sentence:
+#'My hobby is x, my favorite song is y, and I like to eat z.' Where x is hobby, y is fav_song and z is fav_food. .
+
+#3 Spacing with tabs and newlines
+#Declare a variable named my_hero and prints the following sentence using a tab:
+#'My hero is x and his famous quote is y.' Where x is role_model and y is famous_quote.
+
+#Declare a variable named about_me and prints the following sentence that returns a a new line after each numbered item.
+#'This is what I like: 1. hobby 2. fav_color 3. fav_song
+
+
+#4 Variables with a Number value
+#Declare variables named age, weight, shoe_size, fav_number, yen_rate, bitcoin_value, todays_temperature, hawaii_popuation, countries_traveled, number_of_siblings
+
+#Assign your own number values to each variable and print each variable.
+
+#5 Number Concatenation
+
+#6 Variables with a List value
+#Declare a variable named plateLunch and assign it a list containing 5 of your favorite lunch items.
+
+# Declare a variable named `donutBox` and assign it a list containing 5 donuts of your choosing.
+
+# Declare a variable named `westCoast` and assign it a list containing states found on the west coast of the US.
+
+#Declare a variable named `lotteryResult and assign it a list containing 5 random numbers.
+
+#Declare a variable named `hamajang` and assign it a list containing six different data types.
+
+#Declare a variable named `dynamicDuos` and assign it a list containing 3 arrays, with each list containing items goes well with each other. 
+
+# Print each variable.
+
+#7 Accessing values in Arrays
+# * var partyList = ["carrot cake", "gin & tonic", "kalua pork nachos", "double stuff oreos", "cool ranch doritos", "orange chicken"];
+
+# Console.log the entire array.
+# Console.log the length of this array.
+# Console.log only "carrot cake" from this array.
+# Console.log only "cool ranch doritos" from this array
+# */
+
+#8 Variables with a Dictionary value
+
+#9 Variable with a Tuple value
+
+#10 Variables with a Boolean value
+
+
+
