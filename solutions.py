58543cc1aa740f7c2b64849450692a653876be97
Victor <viclee02@gmail.com>
2018-05-06 00:20:50 -1000

    adds list solutions

--- solutions.py
+++ solutions.py
@@ -93,4 +93,75 @@ print(countries_traveled)
 number_of_siblings = 2
 print(number_of_siblings)
 
+#5 Number as Strings Concatenation
+#Declare a variable named self_intro that creates the following sentence:
+#'Aloha, my name is x and I am y years old and my shoe size is z.' Where x is full_name, y is age and z is shoe_size. Print the self_intro variable.
 
+self_intro = 'Aloha, my name is ' + full_name + ' and I am ' + str(age) + ' years old and my shoe size is ' + str(shoe_size) + '.'
+print(self_intro)
+
+#Declare a variable named market_update that creates the following sentence:
+#"Today's bitcoin value is x and the yen exchange rate is y." Where x is bitcoin_value and y is yen_rate. Print the market_update variable.
+
+market_update = "Today's bitcoin value is " + str(bitcoin_value) + " and the yen exchange rate is " + str(yen_rate) + "."
+print(market_update)
+
+#Declare a variable named about_hawaii that creates the following sentence:
+#"Did you know that Hawaii's population is x and the average temperatue is y?" Where x is hawaii_population and y is todays_temperature. Print the about_hawaii variable.
+
+about_hawaii = "Did you know that Hawaii's population is " + str(hawaii_popuation) + " and the average temperature is " + str(todays_temperature) + "?"
+print(about_hawaii)
+
+#6 Variables with a List value
+#Declare a variable named fab_five and assign it a list containing five of your all time favorite snacks. Print the fab_five variable.
+
+fab_five = ['Oreos', 'carrot cake', 'Pretz', 'peanut butter and chocolate cupcakes', 'pub mix']
+print(fab_five)
+
+#Declare a variable named plate_lunch and assign it a list containing five of your favorite lunch items. Print the plate_lunch variable.
+
+plate_lunch = ['chicken katsu', 'kalbi', 'mac salad', 'meat jun','orange chicken']
+print(plate_lunch)
+
+#Declare a variable named ice_cream and assign it a list containing three of your favorite ice cream flavors. Print the ice_cream variable.
+
+ice_cream = ['cookies and cream', 'brownie', 'chocolate and peanut butter']
+print(ice_cream)
+
+#Declare a variable named west_siiiiide and assign it a list containing states found on the west coast of the US. Print the west_siiiiide variable.
+
+west_siiiiide = ['Washington', 'Oregon', 'California']
+print(west_siiiiide)
+
+#Declare a variable named mega_millions and assign it a list containing the Mega Millions Lottery results for May, 4, 2018
+#https://www.lotteryusa.com/mega-millions/. Print the mega_millions variable
+
+mega_millions = [4, 5, 10, 12, 18, 21]
+print(mega_millions)
+
+#Declare a variable named hamajang and assign it a list containing six different data types. Print the hamajang variable.
+
+hamajang = ['pizza', 100, ['tacos', 'nachos'], {'name': 'Snoopy','breed': 'Beagle'}, ('DevLeague', 'Coding'), True]
+print(hamajang)
+
+#Declare a variable named dynamic_duos and assign it a list containing 3 lists, with each list containing items that pairs well with each other. Print the dynamic_duos variable.
+
+dynamic_duos = [['gin', 'tonic'], ['peanut butter', 'celery'], ['bacon', 'cheeseburger']]
+print(dynamic_duos)
+
+#7 Accessing values in List
+vics_list = ['Hendricks gin', 'Fever Tree tonic', 'Costco pub mix', 'cool ranch doritos', 'oreos', 'Safeway fried chicken', 'Morning Glass coffee']
+
+#Print the entire list.
+print(vics_list)
+#Print the length of the list.
+print(len(vics_list))
+#Print only the first element in the list.
+print(vics_list[0])
+#Print only the last element in the list.
+print(vics_list[6])
+#Print 'Safeway fried chicken'
+print(vics_list[5])
+#Replace 'cool ranch doritos' with 'carrot cake' and print the list.
+vics_list[3] = 'carrot cake'
+print(vics_list)
